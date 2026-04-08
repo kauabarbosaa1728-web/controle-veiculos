8b88239ed26f64160a53bd851e0059eedf173ede
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-04-08 14:17:53 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,27 +1,21 @@
-import psycopg2
-from psycopg2 import pool
+import psycopg
+from psycopg_pool import ConnectionPool
 
-# 🔥 SUA URL DO NEON (já configurada)
-DATABASE_URL = "postgresql://neondb_owner:npg_1jUl6SpdBMNg@ep-little-sky-anfi8c6a-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DATABASE_URL = "postgresql://neondb_owner:npg_1jUl6SpdBMNg@ep-little-sky-anfi8c6a-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
-# 🔥 POOL DE CONEXÃO
-conexao_pool = psycopg2.pool.SimpleConnectionPool(
-    1, 10, DATABASE_URL
-)
+pool = ConnectionPool(conninfo=DATABASE_URL)
 
 def conectar():
-    return conexao_pool.getconn()
+    return pool.getconn()
 
 def devolver_conexao(conn):
-    conexao_pool.putconn(conn)
+    pool.putconn(conn)
 
 
-# 🔥 CRIAR TABELAS AUTOMATICAMENTE
 def criar_banco():
     conn = conectar()
     cursor = conn.cursor()
 
-    # 🚗 TABELA DE VEÍCULOS
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS veiculos (
         id SERIAL PRIMARY KEY,
@@ -30,7 +24,6 @@ def criar_banco():
     )
     """)
 
-    # 🔧 TABELA DE MANUTENÇÕES (já preparada pro próximo módulo 😈)
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS manutencoes (
         id SERIAL PRIMARY KEY,
