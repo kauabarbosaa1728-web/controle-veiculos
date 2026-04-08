2a61c5fe23f50c91b42457afffce082cd1fea8be
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-04-08 14:30:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask
 from veiculos import veiculos_bp
+from manutencoes import manutencoes_bp
 from banco import criar_banco
 
 app = Flask(__name__)
 
-# CRIA TABELA AUTOMATICAMENTE
+# 🔥 CRIA TABELAS AUTOMATICAMENTE
 criar_banco()
 
+# 🔥 REGISTRA ROTAS
 app.register_blueprint(veiculos_bp)
+app.register_blueprint(manutencoes_bp)
 
+# 🔥 HOME
 @app.route("/")
 def home():
     return """
     <h1>🚗 Sistema de Controle de Veículos</h1>
-    <a href="/veiculos">👉 Ir para veículos</a>
+
+    <hr>
+
+    <a href="/veiculos">🚗 Gerenciar Veículos</a><br><br>
+    <a href="/manutencoes">🔧 Registrar Manutenções</a>
+
+    <br><br>
+    <p>Sistema rodando online 🚀</p>
     """
 
 if __name__ == "__main__":
