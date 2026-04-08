4afed3fdcfd42833f3b16eebab6416b3587c84ac
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-04-08 13:43:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
+from veiculos import veiculos_bp
+
 app = Flask(__name__)
+
+app.register_blueprint(veiculos_bp)
 
 @app.route("/")
 def home():
-    return "<h1>🚗 Sistema de Controle de Veículos</h1>"
+    return """
+    <h1>🚗 Sistema de Controle de Veículos</h1>
+    <a href="/veiculos">👉 Ir para veículos</a>
+    """
 
 if __name__ == "__main__":
     app.run()
