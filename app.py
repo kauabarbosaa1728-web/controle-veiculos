35d5f52b71668a69fee867221b1321361ba99d08
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-04-08 13:50:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
-
 from veiculos import veiculos_bp
+from banco import criar_banco
 
 app = Flask(__name__)
+
+# 🔥 CRIA TABELA AUTOMATICAMENTE
+criar_banco()
 
 app.register_blueprint(veiculos_bp)
 
